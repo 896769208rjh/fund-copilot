31218fae22fe839e5daa33a048d6228c4c16ee16
fly <896769208@qq.com>
2026-07-08 11:06:09 +0800

    项目初始化

---
+++ src/main/java/edu/rjh/fundcopilot/FundCopilotApplication.java
@@ -0,0 +1,13 @@
+package edu.rjh.fundcopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundCopilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundCopilotApplication.class, args);
+    }
+
+}
